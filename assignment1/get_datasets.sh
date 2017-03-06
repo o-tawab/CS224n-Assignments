b6a8fe3d02cf44172e3088b11459d3e37152497a
3ammor <o.3ammor@gmail.com>
2017-03-07 00:46:27 +0200

    assignment 1, q1

--- assignment1/get_datasets.sh
+++ assignment1/get_datasets.sh
@@ -16,9 +16,9 @@ rm stanfordSentimentTreebank.zip
 
 # Get 50D GloVe vectors
 if hash wget 2>/dev/null; then
-  wget http://web.stanford.edu/~jamesh93/tmp/glove.6B.50d.txt.zip
+  wget https://web.stanford.edu/~jamesh93/tmp/glove.6B.50d.txt.zip
 else
-  curl -O http://web.stanford.edu/~jamesh93/tmp/glove.6B.50d.txt.zip
+  curl -O https://web.stanford.edu/~jamesh93/tmp/glove.6B.50d.txt.zip
 fi
 unzip glove.6B.50d.txt.zip
 rm glove.6B.50d.txt.zip
